2a90af96e1a5f4639f34974e02a9dfe6be10e93c
evgon <evgon_@mail.ru>
2023-03-15 07:25:12 +0300

    111

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-alert( 'Привет, мир!' );
-alert('Пока')
+
+let container = document.querySelector('#container');
+let block_form = container.querySelector('.block_form');
+let input = block_form.querySelector('.input');
+let name = input.querySelector('.input__text input__text_type_song');
+let art = input.querySelector('.input__text input__text_type_artist');
+
+let elem = document.querySelector('#songs-container');
+
+elem.setAttribute('elem', '1111');
+
